d2cad27b9876396ace3e795b42460ba2cb0d63e3
vatsalgp <vatsalgupta280@gmail.com>
2020-01-17 22:35:57 +0530

    Updates

---
+++ precache-manifest.d898cf292edd9cca957ceb12804a197c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9fc9c60c6595231d0da95ff5951e7b6",
+    "url": "/attendance/index.html"
+  },
+  {
+    "revision": "3403da6386be200082a9",
+    "url": "/attendance/static/css/main.0704a135.chunk.css"
+  },
+  {
+    "revision": "6d93671858fe816456b8",
+    "url": "/attendance/static/js/2.52c450d5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/attendance/static/js/2.52c450d5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3403da6386be200082a9",
+    "url": "/attendance/static/js/main.ec214684.chunk.js"
+  },
+  {
+    "revision": "125d15271708bc9820fd",
+    "url": "/attendance/static/js/runtime-main.d208c7e8.js"
+  }
+]);
